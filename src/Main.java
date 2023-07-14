6b0c3655011f97233c6cf69c65268ce0aa256ccc
vadimKapitan <xxxcap13@gmail.com>
2023-07-15 01:06:51 +0300

    ДЗ готово. Просьба проверить.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,103 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
 
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        double[] arr2 = {1.57, 7.654, 9.986};
+
+        int[] arr3 = {3, 7, 8};
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.println(arr[i]);
+                break;
+            }
+            System.out.print(arr[i] + ",");
+        }
+
+        double[] arr2 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < arr2.length; i++) {
+            if (i == arr2.length - 1) {
+                System.out.println(arr2[i]);
+                break;
+            }
+            System.out.print(arr2[i] + ",");
+        }
+
+        int[] arr3 = {3, 7, 8};
+        for (int i = 0; i < arr3.length; i++) {
+            if (i == arr3.length - 1) {
+                System.out.println(arr3[i]);
+                break;
+            }
+            System.out.print(arr3[i] + ",");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr[i]);
+                break;
+            }
+            System.out.print(arr[i] + ",");
+        }
+
+        double[] arr2 = {1.57, 7.654, 9.986};
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr2[i]);
+                break;
+            }
+            System.out.print(arr2[i] + ",");
+        }
+        int[] arr3 = {3, 7, 8};
+        for (int i = arr3.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr3[i]);
+                break;
+            }
+            System.out.print(arr3[i] + ",");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                int value = arr[i] + 1;
+                System.out.println(value);
+            } else {
+                System.out.println(arr[i]);
+            }
+        }
     }
 }
+
+
